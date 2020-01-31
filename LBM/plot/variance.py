ed9b55216d99e454818773693326a1a05a70b3c4
Ivar haugerud <ivar.haugerud@gmail.com>
2020-01-31 16:23:44 +0100

    added variance calculation

--- LBM/plot/variance.py
+++ LBM/plot/variance.py
@@ -1,11 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import matplotlib
-import pandas as pd 
+import pandas as pd
 import matplotlib.animation as animation
-from scipy import ndimage
+import scipy.integrate as sci
 
 plt.style.use("bmh")
 sns.color_palette("hls", 1)
@@ -16,7 +16,6 @@ matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 datafiles = 100
-#var = np.zeros(datafiles)
 var_in = np.zeros(datafiles)
 Nx = 256
 Ny = 64
@@ -25,30 +24,42 @@ x_axis = np.linspace(0, Nx-1, Nx)
 y_axis = np.linspace(0, Ny-1, Ny)
 
 C = np.zeros((Nx, Ny, datafiles))
-C_r  = np.zeros((datafiles, 2))
+C_r  = np.zeros((datafiles))
 
 for i in range(datafiles):
 	data = np.loadtxt("../data/C_"+str(i)+"_front.txt")
 	C[:, :, i] = (np.reshape(data, (Nx, Ny)))
-	m = np.sum(np.sum(C[:,:,i]))
-	#C_r[i, 0] = np.sqrt((np.sum( np.dot(np.square(x_axis), C[:,:,i]))/m)**2 - (np.sum( np.dot(x_axis, C[:,:,i]))/m)**2)
-	C_r[i, 0] = np.sum(np.std( np.dot(x_axis, C[:,:,i])))/m
-	C_r[i, 1] = np.sqrt((np.sum( np.dot(C[:,:,i], np.square(y_axis)))/m)**2 - (np.sum( np.dot(C[:,:,i], y_axis))/m)**2)
+	summed = np.sum(C[:,:,i], axis=1)
+	C_x = x_axis*summed
+	C_xx = x_axis*C_x
+	m = np.trapz(summed)
+	C_r[i] = np.trapz(C_xx)/m - (np.trapz(C_x)/m)**2
 
-plt.plot(C_r[:, 1], label="y")
-plt.plot(C_r[:, 0], label="x")
+
+x_inject = np.argmax(np.sum(C[:,:,0], axis=0))
+y_inject = np.argmax(np.sum(C[:,:,0], axis=1))
+
+print(x_inject, y_inject)
+at_injection_point = np.zeros(datafiles)
+
+plt.plot(C_r, label="x")
 plt.legend(loc="best")
 plt.show()
 
-
 for i in range(datafiles):
-	data = np.loadtxt("../data/C_"+str(i)+"_front.txt")
+	data = np.loadtxt("../data/C_"+str(i)+"_back.txt")
 	C[:, :, i] = (np.reshape(data, (Nx, Ny)))
-	m = np.sum(np.sum(C[:,:,i]))
-	C_r[i, 0] = np.sqrt((np.sum( np.dot(np.square(x_axis), C[:,:,i]))/m)**2 - (np.sum( np.dot(x_axis, C[:,:,i]))/m)**2)
-	C_r[i, 1] = np.sqrt((np.sum( np.dot(C[:,:,i], np.square(y_axis)))/m)**2 - (np.sum( np.dot(C[:,:,i], y_axis))/m)**2)
+	summed = np.sum(C[:,:,i], axis=1)
+	C_x = x_axis*summed
+	C_xx = x_axis*C_x
+	m = np.trapz(summed)
+	C_r[i] = np.trapz(C_xx)/m - (np.trapz(C_x)/m)**2
 
-plt.plot(C_r[:, 1], label="y")
-plt.plot(C_r[:, 0], label="x")
+	at_injection_point[i] = C[x_inject, y_inject, i] + C[x_inject+1, y_inject, i] + C[x_inject-1, y_inject, i] + C[x_inject, y_inject+1, i] + C[x_inject, y_inject-1, i]
+
+plt.plot(C_r, label="x")
 plt.legend(loc="best")
+plt.show()
+
+plt.plot(at_injection_point)
 plt.show()
