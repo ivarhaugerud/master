884e9e240a127b3c70ad12b6c66f6987d8dd2d1c
Ivar haugerud <ivar.haugerud@gmail.com>
2020-03-25 17:03:58 +0100

    entropy is better, but still does not work due to negative consentration

--- LBM/plot/entropy.py
+++ LBM/plot/entropy.py
@@ -30,27 +30,29 @@ u_x = np.reshape(u[0, :], (Nx, Ny))
 
 for i in range(datafiles):
 	data = np.loadtxt("../data/peak_1103_C_"+str(i)+"_1_0.txt")
-	C[:, :, i, 0] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*1e2
+	C[:, :, i, 0] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1103_C_"+str(i)+"_0_5.txt")
-	C[:, :, i, 1] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*1e2
+	C[:, :, i, 1] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1103_C_"+str(i)+"_0_8.txt")
-	C[:, :, i, 2] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*1e2
+	C[:, :, i, 2] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1103_C_"+str(i)+"_0_9.txt")
-	C[:, :, i, 3] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*1e2
+	C[:, :, i, 3] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1103_C_"+str(i)+"_single_maxima.txt")
-	C[:, :, i, 4] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*1e2
+	C[:, :, i, 4] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1103_C_"+str(i)+"_maxima.txt")
-	C[:, :, i, 5] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*1e2
+	C[:, :, i, 5] = (np.reshape(data, (Nx, Ny)))
+	
 
+#normalizing
 for f in range(len(C[0,0,0,:])):
 	for i in range(datafiles):
-		C[:, :, i, f] /= np.sum(np.sum(C[:,:,i,f]))
-	print(np.sum(np.sum(np.sum(C[:,:,:,f], axis=0),axis=0), axis=0))
+		if np.sum(np.sum(C[:,:,i,f])) != 0:
+			C[:, :, i, f] = abs(C[:, :, i, f])/np.trapz(np.trapz(abs(C[:,:,i,f]), x_axis, axis=0), y_axis)
 
 S_each_y    = np.zeros((datafiles, Ny, len(C[0,0,0,:])))
 mass_each_y = np.zeros((datafiles, Ny, len(C[0,0,0,:])))
@@ -58,22 +60,14 @@ mass = np.zeros((datafiles, len(C[0,0,0,:])))
 S    = np.zeros((datafiles, len(C[0,0,0,:])))
 
 import scipy.integrate as sci 
-plt.figure(2)
-
+C[(np.where(C[:, :, :, :] == 0))] = 1
 for f in range(len(C[0,0,0,:])):
 	for i in range(datafiles):
 		for j in range(Ny):
-			#C[np.where( abs((u_x[:, :])) < 1e-8), i, f] = 0
-
-			S_each_y[i, j, f] = np.trapz((C[:, j, i, f])*np.log((C[:, j, i, f])), x_axis)
+			S_each_y[i, j, f] = -np.trapz((abs(C[:, j, i, f]))*np.log((abs(C[:, j, i, f]))), x_axis)
 			mass_each_y[i, j, f] = np.trapz(C[:, j, i, f], x_axis)
 
 		S[i, f] = np.trapz(S_each_y[i, :, f], y_axis)
-		mass[i, f] = np.trapz(mass_each_y[i, :, f], y_axis)
-	S[:, f] - S[0,f]+1
-
-plt.plot(mass)
-plt.show()
 
 plt.plot(t, S[:, 0], label="No cutoff")
 plt.plot(t, S[:, 1], label="0.5 cutoff")
@@ -82,8 +76,7 @@ plt.plot(t, S[:, 3], label="0.9 cutoff")
 plt.plot(t, S[:, 4], label="Single maximum")
 plt.plot(t, S[:, 5], label="Max for each pos")
 plt.xlabel(r"Time [$T_{max}$]", fontsize=14)
-#plt.yscale("log")
-plt.ylabel(r"Entropy $S = \rho\ln{\rho}$", fontsize=14)
+plt.ylabel(r"Entropy $S = -\int\rho\ln{\rho}$", fontsize=14)
 plt.legend(loc="best", fontsize=12, ncol=1)
 plt.savefig("../powerpoint/figures/entropy_high_D.pdf", bbox_inches="tight")
 os.system('pdfcrop %s %s &> /dev/null &'%("../powerpoint/figures/entropy_high_D.pdf", "../powerpoint/figures/entropy_high_D.pdf"))
@@ -94,37 +87,37 @@ S    = np.zeros((datafiles, len(C[0,0,0,:])))
 
 for i in range(datafiles):
 	data = np.loadtxt("../data/peak_1104_C_"+str(i)+"_1_0.txt")
-	C[:, :, i, 0] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*5*1e2
+	C[:, :, i, 0] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1104_C_"+str(i)+"_0_5.txt")
-	C[:, :, i, 1] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*5*1e2
+	C[:, :, i, 1] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1104_C_"+str(i)+"_0_8.txt")
-	C[:, :, i, 2] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*5*1e2
+	C[:, :, i, 2] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1104_C_"+str(i)+"_0_9.txt")
-	C[:, :, i, 3] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*5*1e2
+	C[:, :, i, 3] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1104_C_"+str(i)+"_single_maxima.txt")
-	C[:, :, i, 4] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*5*1e2
+	C[:, :, i, 4] = (np.reshape(data, (Nx, Ny)))
 
 	data = np.loadtxt("../data/peak_1104_C_"+str(i)+"_maxima.txt")
-	C[:, :, i, 5] = (np.reshape(data, (Nx, Ny))) + np.ones((Nx, Ny))*5*1e2
+	C[:, :, i, 5] = (np.reshape(data, (Nx, Ny)))
 
-for i in range(datafiles):
-	C[:, :, i, 0] /= np.sum(np.sum(C[:,:,i,0]))
-	C[:, :, i, 1] /= np.sum(np.sum(C[:,:,i,1]))
-	C[:, :, i, 2] /= np.sum(np.sum(C[:,:,i,2]))
-	C[:, :, i, 3] /= np.sum(np.sum(C[:,:,i,3]))
-	C[:, :, i, 4] /= np.sum(np.sum(C[:,:,i,4]))
-	C[:, :, i, 5] /= np.sum(np.sum(C[:,:,i,5]))
+for f in range(len(C[0,0,0,:])):
+	for i in range(datafiles):
+		if np.sum(np.sum(C[:,:,i,f])) != 0:
+				C[:, :, i, f] = abs(C[:, :, i, f])/np.trapz(np.trapz(abs(C[:,:,i,f]), x_axis, axis=0), y_axis)
+
+C[(np.where(C[:, :, :, :] == 0))] = 1
 
 for f in range(len(C[0,0,0,:])):
 	for i in range(datafiles):
 		for j in range(Ny):
-			S_each_y[i, j, f] = np.trapz((C[:, j, i, f])*np.log((C[:, j, i, f])), x_axis)
+			S_each_y[i, j, f] = -np.trapz((abs(C[:, j, i, f]))*np.log((abs(C[:, j, i, f]))), x_axis)
+			mass_each_y[i, j, f] = np.trapz(C[:, j, i, f], x_axis)
+
 		S[i, f] = np.trapz(S_each_y[i, :, f], y_axis)
-	S[:, f] - S[0,f]+1
 
 plt.plot(t, S[:, 0], label="No cutoff")
 plt.plot(t, S[:, 1], label="0.5 cutoff")
@@ -134,9 +127,8 @@ plt.plot(t, S[:, 4], label="Single maximum")
 plt.plot(t, S[:, 5], label="Max for each pos")
 plt.xlabel(r"Time [$T_{max}$]", fontsize=14)
 #plt.yscale("log")
-plt.ylabel(r"Entropy $S = \rho\ln{\rho}$", fontsize=14)
+plt.ylabel(r"Entropy $S = -\int\rho\ln{\rho}$", fontsize=14)
 plt.legend(loc="best", fontsize=12, ncol=1)
 plt.savefig("../powerpoint/figures/entropy_low_D.pdf", bbox_inches="tight")
 os.system('pdfcrop %s %s &> /dev/null &'%("../powerpoint/figures/entropy_low_D.pdf", "../powerpoint/figures/entropy_low_D.pdf"))
 plt.show()
-
