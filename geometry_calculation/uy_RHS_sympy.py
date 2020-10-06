86a3327f069a86599333c23ac71462c67e0984b3
ivarhaugerud <ivarsh@student.matnat.uio.no>
2020-10-06 18:14:25 +0200

    checked uy one more time

--- geometry_calculation/uy_RHS_sympy.py
+++ geometry_calculation/uy_RHS_sympy.py
@@ -28,31 +28,17 @@ RHS = RHS.subs(A, 1/(gamma*gamma))
 RHS = RHS.subs(B, P_1*kappa*cosh(kappa)/(gamma*gamma))
 RHS = RHS.subs(C, tanh(gamma)/gamma)
 RHS = RHS.subs(D, kappa*P_1*sinh(kappa)/(gamma*gamma))
-#RHS = RHS.subs(P_1, (gamma*tanh(gamma)/(kappa*cosh(kappa)))/(1-kappa_p*tanh(kappa)/(kappa*tanh(kappa_p))))
-#RHS = RHS.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
-#RHS = RHS.subs(gamma, sqrt(i*omega/Sc))
-"""
-print("added them together \n")
-print("RHS: ", RHS)
-expanded_RHS = expand(RHS)
-print("\n \n Expanded RHS: ", expanded_RHS)
-print("\n \n Simplifty RHS: ", simplify(RHS))
-SIMP = simplify(expanded_RHS)
-print("\n \n Simplifty expanded RHS: ", expand(SIMP))
-print("\n \n Simplifty expanded RHS: ", latex(expand(SIMP)))
-"""
+print("RHS: ", simplify(RHS))
 
-my_RHS = kappa*xi*cosh(kappa*xi)*(gamma*gamma-9*kappa*kappa)/6 +sinh(kappa*xi)*(kappa*kappa-2*gamma*gamma/9) + sinh(kappa)*(3*kappa*kappa*kappa_p*xi*cosh(kappa_p*xi)/2-kappa_p*kappa_p*sinh(kappa_p*xi))/sinh(kappa_p)
+my_RHS = kappa*xi*cosh(kappa*xi)*(gamma*gamma-9*kappa*kappa)/6 + sinh(kappa*xi)*(kappa*kappa-2*gamma*gamma/9) + sinh(kappa)*(3*kappa*kappa*kappa_p*xi*cosh(kappa_p*xi)/2-kappa_p*kappa_p*sinh(kappa_p*xi))/sinh(kappa_p)
 my_RHS *= sin(2*kappa*eta)*P_1*kappa/(gamma*gamma)
 difference = RHS - my_RHS
 
 difference = difference.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
-#print(difference)
-#print(expand(difference))
-print("\n\n\n simplified", simplify(expand(difference)))
+print("\n\n\n Difference my RHS and real RHS: ", simplify(expand(difference)))
 
 
-my_RHS = kappa*xi*cosh(kappa*xi)*(gamma*gamma-9*kappa*kappa)/6 +sinh(kappa*xi)*(kappa*kappa-2*gamma*gamma/9) + sinh(kappa)*(3*kappa*kappa*kappa_p*xi*cosh(kappa_p*xi)/2-kappa_p*kappa_p*sinh(kappa_p*xi))/sinh(kappa_p)
 fy = 2*sinh(kappa*xi)*kappa*kappa*gamma*gamma*(1-gamma*gamma/(3*kappa*kappa))/(3*(gamma*gamma+3*kappa*kappa)**2) + kappa*xi*cosh(kappa*xi)*(gamma*gamma*gamma*gamma/6 - 9*kappa*kappa*kappa*kappa/2 - gamma*gamma*kappa*kappa)/((gamma*gamma+3*kappa*kappa)**2) + sinh(kappa)*kappa_p*xi*cosh(kappa_p*xi)/(2*sinh(kappa_p))
+fy *= sin(2*kappa*eta)*P_1*kappa/(gamma*gamma)
 
-print(simplify( (diff(fy, xi, xi) - (gamma*gamma+4*kappa*kappa)*fy + my_RHS).subs(kappa_p, sqrt(gamma*gamma+kappa*kappa))))
+print("Difference solution F_y(xi) and proper solution: ", simplify( (diff(fy, xi, xi) - (gamma*gamma+4*kappa*kappa)*fy + my_RHS).subs(kappa_p, sqrt(gamma*gamma+kappa*kappa))))
