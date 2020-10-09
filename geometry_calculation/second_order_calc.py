d7dfc9b0c88f5d3ccff1d4f4294ac65ea50c5c45
ivarhaugerud <ivarsh@student.matnat.uio.no>
2020-10-09 16:46:33 +0200

    did second order calculation

--- geometry_calculation/second_order_calc.py
+++ geometry_calculation/second_order_calc.py
@@ -21,8 +21,8 @@ grad1y_P1 = simplify(-sin(kappa*eta)*diff(P1, xi))
 grad0_u2 = - simplify(grad1_u1+grad2_u0)
 
 
+print("\n\n-----------------------------------------------------------------")
 print("CALCULATE P2")
-print("-----------------------------------------------------------------")
 term1 = -gamma*gamma*(grad0_u2) + diff(grad0_u2, xi, xi) + diff(grad0_u2, eta, eta)
 term2 =  diff(laplas2_u0x, eta)
 term3 =  diff(laplas1_u1x, eta) + diff(laplas1_u1y, xi)
@@ -55,8 +55,8 @@ P2 = my_sol_P2
 print("P2 = ", P2)
 print("-----------------------------------------------------------------")
 print("\n\n\n")
-print("CALCULATE U2_x")
 print("-----------------------------------------------------------------")
+print("CALCULATE U2_x")
 #define all the terms
 term1 = laplas2_u0x
 term2 = laplas1_u1x
@@ -73,8 +73,6 @@ RHS = RHS.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
 RHS = simplify(expand(RHS))
 RHS_no_eta = simplify(RHS.subs(eta, pi/(4*kappa)))
 RHS_just_eta = simplify(RHS - RHS_no_eta)
-print("\n\n no eta RHS: ", RHS_no_eta)
-print("\n\n just eta RHS: ", RHS_just_eta)
 
 my_RHS = F0*(gamma*xi*sinh(gamma*xi) + cosh(gamma*xi)/2)/cosh(gamma) + P_1*kappa*kappa*((gamma*gamma-kappa*kappa)*xi*sinh(kappa*xi) - 2*kappa*cosh(kappa*xi))/(2*gamma*gamma) + P_1*kappa*kappa*kappa_p*kappa_p*sinh(kappa)*(xi*sinh(kappa_p*xi) + 2*kappa_p*cosh(kappa_p*xi)/(kappa*kappa))/(2*gamma*gamma*sinh(kappa_p))
 difference = my_RHS- RHS_no_eta
@@ -82,7 +80,6 @@ difference = my_RHS- RHS_no_eta
 difference = difference.subs(P_1, (F0*gamma*tanh(gamma)/(kappa*cosh(kappa)))/(1-kappa_p*tanh(kappa)/(kappa*tanh(kappa_p))))
 difference = difference.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
 difference = simplify(expand(difference))
-
 print("Difference my RHS and theirs (no eta)", difference)
 
 my_sol_non_eta = -F0*xi*xi*cosh(gamma*xi)/(4*cosh(gamma)) + P_1*kappa*kappa*xi*sinh(kappa*xi)/(2*gamma*gamma) - P_1*kappa_p*kappa_p*sinh(kappa)*xi*sinh(kappa_p*xi)/(2*gamma*gamma*sinh(kappa_p))
@@ -90,7 +87,7 @@ difference_sol_x = diff(my_sol_non_eta, xi, xi) - gamma*gamma*my_sol_non_eta+ my
 difference_sol_x = difference_sol_x.subs(P_1, (F0*gamma*tanh(gamma)/(kappa*cosh(kappa)))/(1-kappa_p*tanh(kappa)/(kappa*tanh(kappa_p))))
 difference_sol_x = difference_sol_x.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
 difference_sol_x = simplify(expand(difference_sol_x))
-print(difference_sol_x)
+print("Difference solution (no eta):", difference_sol_x)
 
 my_RHS = -gamma*F0*xi*sinh(gamma*xi)/cosh(gamma) + F0*cosh(gamma*xi)*(kappa*kappa*xi*xi-1/2)/cosh(gamma) - P_1*xi*sinh(kappa*xi)*(3*kappa**4 + gamma*gamma*kappa*kappa)/(2*gamma*gamma) + P_1*kappa*kappa*kappa*cosh(kappa*xi)/(gamma*gamma) - 2*kappa*psi_2*cosh(2*kappa*xi) + 3*P_1*kappa*kappa*kappa_p*kappa_p*sinh(kappa)*xi*sinh(kappa_p*xi)/(2*gamma*gamma*sinh(kappa_p)) - P_1*kappa_p*kappa_p*kappa_p*cosh(kappa_p*xi)*sinh(kappa)/(gamma*gamma*sinh(kappa_p))
 my_RHS *= cos(2*kappa*eta)
@@ -100,12 +97,53 @@ difference = difference.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
 difference = simplify(expand(difference))
 print("Difference my RHS and theirs (just eta)", difference)
 
-my_sol_eta = xi*xi*cosh(gamma*xi)*F0/(4*cosh(gamma)) + P_1*kappa*kappa*kappa*cosh(kappa*xi)/(gamma*gamma*(gamma*gamma+3*kappa*kappa)) - 2*kappa*psi_2*cosh(2*kappa*xi)/(gamma*gamma)  + (P_1*kappa_p*kappa_p*sinh(kappa)/(gamma*gamma*sinh(kappa_p)))*(xi*sinh(kappa_p*xi)/2) + P_1*kappa*kappa*(xi*sinh(kappa*xi)/2 + kappa*cosh(kappa*xi)/(gamma*gamma+3*kappa*kappa))/(gamma*gamma)
+my_sol_eta = xi*xi*cosh(gamma*xi)*F0/(4*cosh(gamma)) - 2*kappa*psi_2*cosh(2*kappa*xi)/(gamma*gamma) + (P_1*kappa_p*kappa_p*sinh(kappa)/(gamma*gamma*sinh(kappa_p)))*(xi*sinh(kappa_p*xi)/2) - P_1*kappa*kappa*xi*sinh(kappa*xi)/(2*gamma*gamma)
 difference_sol_x = diff(my_sol_eta, xi, xi) - (gamma*gamma+4*kappa*kappa)*my_sol_eta + my_RHS/cos(2*kappa*eta)
 
 #difference_sol_x = difference_sol_x.subs(P_1, (F0*gamma*tanh(gamma)/(kappa*cosh(kappa)))/(1-kappa_p*tanh(kappa)/(kappa*tanh(kappa_p))))
 difference_sol_x = difference_sol_x.subs(kappa_p, sqrt(kappa*kappa+gamma*gamma))
 difference_sol_x = simplify(expand(difference_sol_x))
-print(difference_sol_x)
+print("Difference solution (just eta):", difference_sol_x)
+print("u_x2 = ", cos(2*kappa*eta)*my_sol_eta, "+", my_sol_non_eta)
+print("-----------------------------------------------------------------")
+print("\n\n\n")
 
-#-F0*kappa**2*xi*(3*gamma**2*kappa*sinh(kappa)*sinh(xi*sqrt(gamma**2 + kappa**2))*tanh(sqrt(gamma**2 + kappa**2)) + 2*gamma**2*kappa*sinh(kappa*xi)*sinh(sqrt(gamma**2 + kappa**2))*tanh(sqrt(gamma**2 + kappa**2)) - 3*gamma**2*sqrt(gamma**2 + kappa**2)*sinh(kappa)*sinh(xi*sqrt(gamma**2 + kappa**2))*tanh(kappa) - 2*gamma**2*sqrt(gamma**2 + kappa**2)*sinh(kappa*xi)*sinh(sqrt(gamma**2 + kappa**2))*tanh(kappa) + 3*kappa**3*sinh(kappa)*sinh(xi*sqrt(gamma**2 + kappa**2))*tanh(sqrt(gamma**2 + kappa**2)) + 6*kappa**3*sinh(kappa*xi)*sinh(sqrt(gamma**2 + kappa**2))*tanh(sqrt(gamma**2 + kappa**2)) - 3*kappa**2*sqrt(gamma**2 + kappa**2)*sinh(kappa)*sinh(xi*sqrt(gamma**2 + kappa**2))*tanh(kappa) - 6*kappa**2*sqrt(gamma**2 + kappa**2)*sinh(kappa*xi)*sinh(sqrt(gamma**2 + kappa**2))*tanh(kappa))*tanh(gamma)*tanh(sqrt(gamma**2 + kappa**2))/(2*gamma*(gamma**2*tanh(kappa)**2 + kappa**2*tanh(kappa)**2 + kappa**2*tanh(sqrt(gamma**2 + kappa**2))**2 - 2*kappa*sqrt(gamma**2 + kappa**2)*tanh(kappa)*tanh(sqrt(gamma**2 + kappa**2)))*sinh(sqrt(gamma**2 + kappa**2))*cosh(kappa))
+print("-----------------------------------------------------------------")
+print("CALCULATE U2_y")
+
+#define all the terms
+term1 = laplas1_u1y
+term2 = -grad1y_P1
+term3 = -diff(P2, xi)
+
+#add together, the substitute back and simplify
+RHS = term1 + term2 + term3
+RHS = RHS.subs(B, P_1*kappa*cosh(kappa)/(gamma*gamma))
+RHS = RHS.subs(C, F0*tanh(gamma)/gamma)
+RHS = RHS.subs(D, kappa*P_1*sinh(kappa)/(gamma*gamma))
+
+my_RHS = P_1*kappa*kappa*kappa*(2*sinh(kappa*xi) - (3*kappa*kappa+gamma*gamma)*xi*cosh(kappa*xi)/kappa)/(2*gamma*gamma) + P_1*kappa*kappa_p*sinh(kappa)*(3*kappa*kappa*xi*cosh(kappa_p*xi)-2*kappa_p*sinh(kappa_p*xi))/(2*gamma*gamma*sinh(kappa_p)) - 2*kappa*psi_2*sinh(2*kappa*xi)
+my_RHS *= sin(2*kappa*eta)
+print("Differnece my RHS and correct RHS:", simplify(expand(my_RHS-RHS)))
+
+my_sol_y = P_1*kappa*kappa_p*sinh(kappa)*xi*cosh(kappa_p*xi)/(2*gamma*gamma*sinh(kappa_p)) - P_1*kappa*kappa*xi*cosh(kappa*xi)/(2*gamma*gamma) - 2*kappa*psi_2*sinh(2*kappa*xi)/(gamma*gamma)
+difference_sol_y = diff(my_sol_y, xi, xi) - (gamma*gamma + 4*kappa*kappa)*my_sol_y + my_RHS/sin(2*kappa*eta)
+difference_sol_y = simplify(expand(difference_sol_y.subs(kappa_p, sqrt(gamma*gamma+kappa*kappa))))
+print("Difference solution :", difference_sol_y)
+print("u_y2 =", my_sol_y*sin(2*kappa*eta))
+print("-----------------------------------------------------------------")
+print("\n\n\n")
+
+print("-----------------------------------------------------------------")
+print("CALCULATE divergence")
+RHS = -grad1_u1- grad2_u0
+LHS = diff(my_sol_y, xi)*sin(2*kappa*eta) + diff(my_sol_eta*cos(2*kappa*eta), eta)
+difference = LHS - RHS 
+difference = difference.subs(B, P_1*kappa*cosh(kappa)/(gamma*gamma))
+difference = difference.subs(C, F0*tanh(gamma)/gamma)
+difference = difference.subs(D, kappa*P_1*sinh(kappa)/(gamma*gamma))
+difference = difference.subs(P_1, (F0*gamma*tanh(gamma)/(kappa*cosh(kappa)))/(1-kappa_p*tanh(kappa)/(kappa*tanh(kappa_p))))
+difference = difference.subs(kappa_p, sqrt(gamma*gamma+kappa*kappa))
+
+difference = simplify(expand(difference))
+print(difference)
