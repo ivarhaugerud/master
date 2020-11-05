798a60e3f9692c38fc4bda294cf08ca4478c1791
ivarhaugerud <ivarsh@student.matnat.uio.no>
2020-11-05 17:12:42 +0100

    second order became horrible, but wont contribute..

--- geometry_calculation/B1_calc.py
+++ geometry_calculation/B1_calc.py
@@ -8,33 +8,38 @@ omega, gamma, Gamma, kappa, kappa_p, Sc, F0, Pe, rho, rho_p = symbols("omega gam
 B0 = Pe*F0*tanh(gamma)/(gamma*gamma*gamma*rho*rho) + Pe*F0*tanh(gamma)/(gamma*gamma*gamma*(Sc-1))*(cosh(rho*xi)/(rho*sinh(rho)) - cosh(gamma*xi)/(gamma*sinh(gamma)))
 
 
-print("\n\n-----------------------------------------------------------------")
-print("CALCULATE B1 low Pe")
+print("\n-----------------------------------------------------------------")
+print("CALCULATE B1")
+
+#calculate RHS
 RHS = kappa*kappa*xi*diff(B0, xi) - 2*diff(B0, xi, xi)
 
-B1_extra = -cosh(kappa*xi)*cos(kappa*eta)/sinh(kappa)
-
+#test particular solution
 B1_partic = (Pe*F0*tanh(gamma)/(gamma*gamma*gamma*(Sc-1)))*(xi*sinh(rho*xi)/sinh(rho) + (kappa*kappa/(gamma*gamma-rho_p*rho_p))*(xi*sinh(gamma*xi)/sinh(gamma)) + 2*gamma*(rho*rho-gamma*gamma)/((gamma*gamma-rho_p*rho_p)*(gamma*gamma-rho_p*rho_p)*tanh(gamma))*(cosh(gamma*xi)/cosh(gamma)))
-should_be_zero = simplify( -diff(B1_partic, xi, xi) + (rho*rho+kappa*kappa)*B1_partic - RHS)
-should_be_zero = simplify(expand(should_be_zero.subs(rho_p, sqrt(rho*rho+kappa*kappa))))
-print("Test homogeneous + particular solution: ", should_be_zero)
+#should_be_zero = simplify( -diff(B1_partic, xi, xi) + (rho*rho+kappa*kappa)*B1_partic - RHS)
+#should_be_zero = simplify(expand(should_be_zero.subs(rho_p, sqrt(rho*rho+kappa*kappa))))
+#print("Test particular solution: ", should_be_zero)
 
-derivative     = simplify(diff(B1_partic, xi))
-boundary_value = simplify(derivative.subs(xi, 1))
-boundary_value = simplify(expand(boundary_value.subs(rho_p, sqrt(rho*rho+kappa*kappa))))
-
-
-
-g0 = -(Pe*F0*tanh(gamma)/(rho_p*sinh(rho_p)*gamma*gamma*gamma*(Sc-1)))*(1 + rho/tanh(rho) + (kappa*kappa/(gamma*gamma-rho_p*rho_p))*(1+gamma/tanh(gamma)) + 2*gamma*gamma*(rho*rho-gamma*gamma)/((gamma*gamma-rho_p*rho_p)*(gamma*gamma-rho_p*rho_p)))
-B1_homo = g0*cosh(rho_p*xi)
-
-derivative = simplify(diff(B1_homo+B1_partic+B1_extra, xi))
+#test Boundary condition
+B1_homo_1 = -(Pe*F0*tanh(gamma)/(rho_p*sinh(rho_p)*gamma*gamma*gamma*(Sc-1)))*(1 + rho/tanh(rho) + (kappa*kappa/(gamma*gamma-rho_p*rho_p))*(1+gamma/tanh(gamma)) + 2*gamma*gamma*(rho*rho-gamma*gamma)/((gamma*gamma-rho_p*rho_p)*(gamma*gamma-rho_p*rho_p)))*cosh(rho_p*xi)
+B1_homo_2 = 0#-cos(kappa*eta)*cosh(kappa*xi)/sinh(kappa)
+"""
+derivative = simplify(diff(B1_homo_1+B1_partic+B1_homo_2, xi))
 boundary_value = simplify(derivative.subs(xi, 1))
 boundary_value = simplify(expand(boundary_value.subs(rho_p, sqrt(rho*rho+kappa*kappa))))
 print("\n Test boundary condition: ", boundary_value)
+"""
+B1 = simplify(B1_homo_2 + B1_homo_1+ B1_partic)
 
+"""
 
+print("\n-----------------------------------------------------------------")
+print("CALCULATE B2")
+RHS = -0.5*(kappa*kappa*xi*diff(B1, xi)-2*diff(B1, xi, xi)) - (kappa*kappa*xi*diff(B0, xi) + 3*diff(B0, xi, xi) + kappa*kappa*xi*xi*diff(B0, xi, xi))
+#RHS = simplify(expand(RHS.subs(rho_p, sqrt(rho*rho+kappa*kappa))))
+RHS = simplify(expand(RHS))
 
-
-
-g0 -F0*Pe*(rho*(gamma**4 - 2*gamma**2*kappa**2 - 2*gamma**2*rho**2 + kappa**4 + 2*kappa**2*rho**2 + rho**4)*tanh(gamma) - (gamma**4*sqrt(kappa**2 + rho**2)*tanh(gamma)*tanh(sqrt(kappa**2 + rho**2)) + gamma**4*tanh(gamma) - gamma**3*kappa**2 - 2*gamma**3*sqrt(kappa**2 + rho**2)*tanh(sqrt(kappa**2 + rho**2)) - gamma**2*kappa**2*sqrt(kappa**2 + rho**2)*tanh(gamma)*tanh(sqrt(kappa**2 + rho**2)) + gamma**2*kappa**2*tanh(gamma) - 2*gamma**2*rho**2*sqrt(kappa**2 + rho**2)*tanh(gamma)*tanh(sqrt(kappa**2 + rho**2)) + gamma*kappa**4 + gamma*kappa**2*rho**2 + 2*gamma*rho**2*sqrt(kappa**2 + rho**2)*tanh(sqrt(kappa**2 + rho**2)) + kappa**2*rho**2*sqrt(kappa**2 + rho**2)*tanh(gamma)*tanh(sqrt(kappa**2 + rho**2)) - kappa**2*rho**2*tanh(gamma) + rho**4*sqrt(kappa**2 + rho**2)*tanh(gamma)*tanh(sqrt(kappa**2 + rho**2)) - rho**4*tanh(gamma))*tanh(rho))/(gamma**3*rho_p*(Sc*gamma**4 - 2*Sc*gamma**2*kappa**2 - 2*Sc*gamma**2*rho**2 + Sc*kappa**4 + 2*Sc*kappa**2*rho**2 + Sc*rho**4 - gamma**4 + 2*gamma**2*kappa**2 + 2*gamma**2*rho**2 - kappa**4 - 2*kappa**2*rho**2 - rho**4)*sinh(rho_p)*tanh(rho))
+print("\n\n", simplify(RHS))
+"""
+print("\n-----------------------------------------------------------------")
+print("CALCULATE D_parallel")
