53994295da44cf52bcae183ebd5c64697328b5ed
ivarhaugerud <ivarsh@student.matnat.uio.no>
2020-11-20 16:17:15 +0100

    still looking good

--- geometry_calculation/finite_element/solve_B1.py
+++ geometry_calculation/finite_element/solve_B1.py
@@ -46,7 +46,7 @@ def coupled_finite_element_solver(N, n, x, alpha, couple_forward, couple_backwar
 	for i in range(n-1):
 		A[(i+1)*N : (i+2)*N, i*N    :(i+1)*N] += couple_forward  *np.identity(N)*Delta_x
 		A[i*N     : (i+1)*N, (i+1)*N:(i+2)*N] += couple_backward *np.identity(N)*Delta_x
-	print(A)
+
 	#calculate source vector
 	for j in range(n):
 		for i in range(N):
@@ -67,14 +67,14 @@ def coupled_finite_element_solver(N, n, x, alpha, couple_forward, couple_backwar
 			u[j,:] += sol[j*N+i]*phi[i, :]
 	return u
 
-
-k = np.arange(-3, 3, 1)
+tol = 1e-6
+k = np.arange(-2, 2, 1)
 xi = np.linspace(-1, 1, int(1e5))
 q = np.zeros((len(k), len(xi)), dtype="complex")
 
 kappa = 0.5
 Sc = 1.2 
-omega = 1.35*100
+omega = 1.35
 F0 = 3
 Pe = F0/Sc 
 
@@ -108,7 +108,7 @@ Delta = xi[1]-xi[0]
 
 #works for differential equation with constant terms, now just need coupeling to work as well
 n = len(k) #number of vectors
-N = 100 #length of each vector 
+N = 500 #length of each vector 
 N_pos = np.linspace(min(xi), max(xi), N)
 
 alpha = p_np2 #self-interaction
@@ -137,16 +137,102 @@ couple_forward[-1] *= 0.5
 Bc0 = np.zeros(n, dtype="complex")
 Bc1 = np.zeros(n, dtype="complex")
 
+sol = coupled_finite_element_solver(N, n, xi, alpha, couple_backward, couple_forward, q, Bc0, Bc1)
+
+
+for i in range(int(len(k)/2)+1):
+	if k[i] < 1e-3:
+		plt.plot(xi, -np.real(sol[i,0]+sol[-i,0]) + np.real(sol[i,:]+sol[-i,:]), label=r"$\omega=\omega$"+str(abs(k[i])))
+		
+		if np.max(abs(np.imag(sol[i,:]+sol[-i,:]) - np.imag(sol[i,0]+sol[-i,0]))) > tol:
+			print("LARGE IMAGINARY VALUE FOR " + str(k[i]) + "-OMEGA = ", np.max(abs(np.imag(sol[i,:]+sol[-i,:]) - np.imag(sol[i,0]+sol[-i,0]))))
+	else:
+		plt.plot(xi, np.real(sol[0,:]), label=str(k[i]))
+
+		if np.max(abs(np.imag(sol[0,:]-sol[0,0]))) > tol:
+			print("LARGE IMAGINARY VALUE FOR 0-OMEGA = ", np.max(abs(np.imag(sol[0,:]))))
+
+plt.figure(1)
+plt.legend(loc="best")
+plt.show()
+
+
+
+
+
+
+
+###
+
+
+
+
+k = np.arange(-3, 3, 1)
+q = np.zeros((len(k), len(xi)), dtype="complex")
+
+kappa = 0.5
+Sc = 1.2 
+omega = 1.35
+F0 = 3
+Pe = F0/Sc 
+
+q[np.argmin(abs(k+2)), :] = kappa*xi*np.conj(ux0)*np.conj(B0_deriv)/4 - np.conj(uy1)*np.conj(B0_deriv)/4
+#q[np.argmin(abs(k+1)), :] = kappa*kappa*xi*np.conj(B0_deriv)/2 - 2*np.conj(B0_deriv_deriv)/2 + Pe*np.conj(ux1)/2
+q[np.argmin(abs(k-0)), :] = kappa*xi*(ux0*np.conj(B0_deriv) + np.conj(ux0)*B0_deriv) - uy1*np.conj(B0_deriv) - np.conj(uy1)*B0_deriv
+#q[np.argmin(abs(k-1)), :] = kappa*kappa*xi*B0_deriv/2 - 2*B0_deriv_deriv/2 + Pe*ux1/2
+q[np.argmin(abs(k-2)), :] = kappa*xi*ux0*B0_deriv/4 - uy1*B0_deriv/4
+
+
+p_np2 = 1j*omega*k + kappa*kappa
+Delta = xi[1]-xi[0]
+
+#works for differential equation with constant terms, now just need coupeling to work as well
+n = len(k) #number of vectors
+N = 500 #length of each vector 
+N_pos = np.linspace(min(xi), max(xi), N)
+
+alpha = p_np2 #self-interaction
+
+#coupleing between vectors
+couple_forward  =  np.zeros(N, dtype="complex")
+couple_backward =  np.zeros(N, dtype="complex")
+
+for i in range(N):
+	couple_backward[i] = -Delta*(ux0[np.argmin(abs(xi-(N_pos[i]+Delta/2)))] + ux0[np.argmin(abs(xi-(N_pos[i]-Delta/2)))])/2
+
+couple_backward[0]   = -Delta*(ux0[np.argmin(abs(xi-(N_pos[0] + Delta/2)))])/2
+couple_backward[-1]  = -Delta*(ux0[np.argmin(abs(xi-(N_pos[-1]- Delta/2)))])/2
+couple_backward *= kappa 
+couple_forward = np.conj(couple_backward)
+
+#since integral over last and first basis function is half the value of the others
+couple_backward[0]  *= 0.5
+couple_backward[-1] *= 0.5 
+
+couple_forward[0]  *= 0.5
+couple_forward[-1] *= 0.5 
+
+#boundary conditions
+Bc0 = np.zeros(n, dtype="complex")
+Bc1 = np.zeros(n, dtype="complex")
+
+Bc0[np.argmin(abs(k-0))] =  kappa
+Bc1[np.argmin(abs(k-0))] = -kappa
 
 sol = coupled_finite_element_solver(N, n, xi, alpha, couple_backward, couple_forward, q, Bc0, Bc1)
 
-for i in range(int((len(sol[:,0])-1)/2)+1):
-	plt.figure(1)
-	plt.plot(xi, -np.real(sol[i,0]+sol[-i,0]) + np.real(sol[i,:]+sol[-i,:]), label="num"+str(k[i]))
+for i in range(int(len(k)/2)+1):
+	if k[i] < 1e-3:
+		plt.plot(xi, -np.real(sol[i,0]+sol[-i,0]) + np.real(sol[i,:]+sol[-i,:]), label=r"$\omega=\omega$"+str(abs(k[i])))
+		
+		if np.max(abs(np.imag(sol[i,:]+sol[-i,:]) - np.imag(sol[i,0]+sol[-i,0]))) > tol:
+			print("LARGE IMAGINARY VALUE FOR " + str(k[i]) + "-OMEGA = ", np.max(abs(np.imag(sol[i,:]+sol[-i,:]) - np.imag(sol[i,0]+sol[-i,0]))))
+	else:
+		plt.plot(xi, np.real(sol[0,:]), label=str(k[i]))
 
-	plt.figure(2)
-	print(k[i], k[-i])
-	plt.plot(xi, np.imag(sol[i,:]+sol[-i, :]), label="num"+str(k[i]))
+		if np.max(abs(np.imag(sol[0,:]-sol[0,0]))) > tol:
+			print("LARGE IMAGINARY VALUE FOR 0-OMEGA = ", np.max(abs(np.imag(sol[0,:]))))
 
+plt.figure(1)
 plt.legend(loc="best")
 plt.show()
