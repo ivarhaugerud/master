b3614d6151ded2727925a9cac0739f82792a73b0
ivarhaugerud <ivarsh@student.matnat.uio.no>
2020-11-20 12:10:14 +0100

    close to something

--- geometry_calculation/finite_element/FE_coupled_eq.py
+++ geometry_calculation/finite_element/FE_coupled_eq.py
@@ -59,9 +59,6 @@ def coupled_finite_element_solver(N, n, x, alpha, couple_forward, couple_backwar
 		b[N*j+0]    -= Bc0[j]
 		b[N*j+N-1]  += Bc1[j]
 
-	print(A+A_p)
-	print(b)
-
 	sol = np.linalg.solve(A+A_p, b)
 
 	#transfer back solution to regular basis
@@ -70,10 +67,7 @@ def coupled_finite_element_solver(N, n, x, alpha, couple_forward, couple_backwar
 			u[j,:] += sol[j*N+i]*phi[i, :]
 	return u
 
-
-
-
-
+"""
 #works for differential equation with constant terms, now just need coupeling to work as well
 n = 2  #number of vectors 
 N = 500 #length of each vector 
@@ -121,7 +115,6 @@ plt.plot(x,  np.sin(np.pi*x)/(1+np.pi*np.pi), "--")
 plt.legend(loc="best")
 plt.show()
 
-"""
 #works for differential equation with constant terms, now just need coupeling to work as well
 n = 2  #number of vectors 
 N = 500 #length of each vector 
@@ -158,6 +151,61 @@ plt.plot(x, -np.sin(np.pi*x), "--")
 #plt.plot(x, f[1,:], "--")
 plt.legend(loc="best")
 plt.show()
-"""
 
 #tomorrow try and solve f'' = g and g'' = (1+pi*pi)*cos(k*x)
+"""
+
+
+#works for differential equation with constant terms, now just need coupeling to work as well
+n = 2  #number of vectors 
+N = 500 #length of each vector 
+x = np.linspace(0, 1, int(1e4))
+
+alpha = np.zeros(n) #self-interaction
+
+alpha[0] =  -3
+alpha[1] =  -3
+
+#coupleing between vectors
+couple_backward = -2*np.ones(N)
+couple_forward  =  2*np.ones(N)
+
+
+sol1 = np.real(-1j*np.sqrt(3)/2*np.exp(1j*np.sqrt(3)/2*x) + 1j*np.sqrt(3)/2*np.exp(-1j*np.sqrt(3)/2*x))
+sol2 = 3/2*np.real(np.exp(1j*np.sqrt(3)/2*x) + np.exp(-1j*np.sqrt(3)/2*x))
+
+#since integral over last and first basis function is half the value of the others
+couple_backward[0]  *= 0.5
+couple_backward[-1] *= 0.5 
+
+couple_forward[0]  *= 0.5
+couple_forward[-1] *= 0.5 
+
+#boundary conditions
+Bc0 = np.zeros(n)
+Bc1 = np.zeros(n)
+
+plt.plot(x, sol1)
+plt.plot(x, sol2)
+plt.show()
+
+Bc0[0] = np.gradient(sol1, x)[0]
+Bc1[0] = np.gradient(sol1, x)[-1]
+
+Bc0[1] = np.gradient(sol2, x)[0]
+Bc1[1] = np.gradient(sol2, x)[-1]
+
+f = np.zeros((n, len(x)))
+#f[0, :] =  (1+np.pi*np.pi)*np.sin(np.pi*x)
+#f[1, :] =  (1+np.pi*np.pi)*np.sin(np.pi*x)
+
+
+sol = coupled_finite_element_solver(N, n, x, alpha, couple_backward, couple_forward, f, Bc0, Bc1)
+
+for i in range(len(sol[:,0])):
+	plt.plot(x, sol[i,:], label="num"+str(i))
+
+plt.plot(x, sol1, "--")
+plt.plot(x, sol2, "--")
+plt.legend(loc="best")
+plt.show()
