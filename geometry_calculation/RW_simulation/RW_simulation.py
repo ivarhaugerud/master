the correct scaling for the kinetic enery does not produce correct value for effective dispersion

geometry_calculation/RW_simulation/RW_simulation.py
@@ -36,11 +36,11 @@ prev_pos[1,:] = np.copy(pos[1,:])
 
 xi = np.linspace(-1, 1, int(1e5))
 nu = 0.5
-F0 = 3.0
+F0 = 3.0/nu
 omega = 2*np.pi/tau 
 Sc = nu
 gamma = np.sqrt(1j*omega/Sc)
-ux = F0*(1-np.cosh(gamma*xi)/np.cosh(gamma))/(gamma*gamma)
+ux = F0*(1-np.cosh(gamma*xi)/np.cosh(gamma))/(gamma*gamma)/2
 U_ana = integrate.trapz(ux*np.conj(ux), xi)
 
 
@@ -51,15 +51,14 @@ for j in range(len(kappas)):
 
     time = tdat[:,0]
     u2   = tdat[:,4]
-    print(np.mean(u2), np.mean(tdat[:,8]))
     exp_u2[j] = integrate.trapz(u2[-timesteps:], time[-timesteps:])/(tau)
-    print(np.mean(u2[-timesteps:]))
+    #print(np.mean(u2[-timesteps:]))
     plt.plot(time, u2)
     plt.plot(time[-timesteps:], u2[-timesteps:])
-    print("U = ", np.sqrt(exp_u2[j]))
-    print("U_ana = ", np.sqrt(U_ana))
+    print("U = ", (exp_u2[j]))
+    print("U_ana = ", (U_ana))
     plt.show()
-
+"""
 timesteps = int(timesteps/skip)
 
 for i in range(len(Lx)):
@@ -110,3 +109,4 @@ for i in range(len(Lx)):
         if int(k) % int(periods*timesteps/datafiles) == 0:
             #np.save('data/Lx62_8/RW_positions_' +str(k), pos[:, :])
             np.save(dirr+'pos_2/RW_positions_' +str(k), pos[:, :])
+"""

geometry_calculation/RW_simulation/plot_pos.py
@@ -6,21 +6,23 @@ dt = 0.002
 tau = 3.0 
 
 epsilon = 0.0
-U_scale = 1/4.0
-D       = 2.0/(3.0)
-Pe      = U_scale/D
+U_scale = 5
+Pe = 20
+D       = U_scale/Pe 
+
+#Pe      = #U_scale/D
 
 kappas = np.array([0.4])
 Lx = 2*np.pi/kappas
 
 omega = 2*np.pi/tau
-nu = 4.0
+nu = 4.0	
 F0 = 3/nu
 Sc = nu
 gamma = np.sqrt(1j*omega/Sc)
 timesteps = int(tau/dt)
 period    = tau
-
+print(D)
 periods = 5000
 datafiles = periods*25
 skip = int(periods*timesteps/datafiles)
@@ -47,7 +49,7 @@ y = np.zeros(datafiles)
 
 for i in range(datafiles):
 	#plt.clf()
-	pos = np.load(dirr+"pos/RW_positions_"+str(int(i*skip))+".npy")
+	pos = np.load(dirr+"pos_2/RW_positions_"+str(int(i*skip))+".npy")
 	#plt.title(str(t[int(i)]))
 	#plt.scatter(pos[0, :], pos[1, :])
 	#plt.axis([-4, 4, -1, 1])
@@ -60,7 +62,7 @@ for i in range(datafiles):
 print(np.mean(var[int(datafiles/2):]/t[int(datafiles/2):]))
 print(np.mean(np.std((var[int(datafiles/2):]/t[int(datafiles/2):]))))
 
-np.save(dirr+"pos/var", var)
+np.save(dirr+"pos_2/var", var)
 
 plt.plot(t, mean)
 plt.show()
@@ -68,7 +70,6 @@ plt.show()
 plt.plot(x, y)
 plt.show()
 
-var = np.load(dirr+"pos/var.npy")
 plt.plot(t/tau, var)
 plt.xlabel("time [periods]")
 plt.ylabel("variance")

geometry_calculation/RW_simulation/plot_vs_gamma.py
@@ -5,27 +5,26 @@ import scipy.integrate as sci
 T = 3
 omega = 2*np.pi/T
 epsilon = 0.0
-U_scale = 1/2.0
-Dm = 1/3.0
-Pe = U_scale/Dm
+U_scale = 5
+Pe = 20 
+Dm = U_scale/Pe
 
 periods = 5000
 datafiles = periods*25
 half_way = int(datafiles/2)
-t = np.linspace(0, 0.5*T*periods, datafiles)
+t = np.linspace(0, T*periods, datafiles)
 
 nus = np.array([0.5, 4.0])
 
 var    = np.zeros((len(nus), datafiles))
 D_para = np.zeros((len(nus), datafiles-1))
 D      = np.zeros((len(nus), 2))
-
-
-
 D_ana = np.zeros(len(nus))
+
+
 for i in range(len(nus)):
-	Sc = nus[i]#*Pe
-	F0 = 3#/nus[i]
+	Sc = nus[i]#/Dm
+	F0 = 3/nus[i]
 	gamma   = np.sqrt(1j*omega/Sc)
 	gamma_c = np.conj(gamma)
 	a       = np.sqrt(1j*omega)
@@ -33,10 +32,10 @@ for i in range(len(nus)):
 	xi      = np.linspace(-1, 1, int(1e5))
 
 	factor  = Sc*Sc*Sc*Pe*Pe*F0*F0*np.tanh(gamma)*np.tanh(gamma_c)/(omega*omega*omega*(Sc-1)*(Sc-1))
-	D_ana[i] = 1 + np.real(factor * 0.5 * sci.trapz( np.sinh(a*xi)*np.sinh(a_c*xi)/(np.sinh(a)*np.sinh(a_c)) + np.sinh(gamma*xi)*np.sinh(gamma_c*xi)/(np.sinh(gamma)*np.sinh(gamma_c)) - np.sinh(a*xi)*np.sinh(gamma_c*xi)/(np.sinh(a)*np.sinh(gamma_c)) - np.sinh(gamma*xi)*np.sinh(a_c*xi)/(np.sinh(gamma)*np.sinh(a_c)), xi))
+	D_ana[i] = Dm*(1 + np.real(factor * 0.5 * sci.trapz( np.sinh(a*xi)*np.sinh(a_c*xi)/(np.sinh(a)*np.sinh(a_c)) + np.sinh(gamma*xi)*np.sinh(gamma_c*xi)/(np.sinh(gamma)*np.sinh(gamma_c)) - np.sinh(a*xi)*np.sinh(gamma_c*xi)/(np.sinh(a)*np.sinh(gamma_c)) - np.sinh(gamma*xi)*np.sinh(a_c*xi)/(np.sinh(gamma)*np.sinh(a_c)), xi)))
 
 for i in range(len(nus)):
-	var[i, :] = np.load("flow_fields/zero_eps/mu_"+str(nus[i]) +"/pos/var.npy")/Dm #*np.sqrt(2)
+	var[i, :] = np.load("flow_fields/zero_eps/mu_"+str(nus[i]) +"/pos_2/var.npy")
 """
 
 for i in range(len(nus)):