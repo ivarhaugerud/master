b8a8a33d5b443f21797d1716b3fd3407017e31dd
ivarhaugerud <ivarsh@student.matnat.uio.no>
2021-01-25 12:56:19 +0100

    cleaned up some code

--- geometry_calculation/sympy/test_taylor_B1.py
+++ geometry_calculation/sympy/test_taylor_B1.py
@@ -14,8 +14,6 @@ print(simplify((diff(sol, xi).subs(xi, 1))))
 
 
 # SOLVING FOR B2
-"""
-A = (F0/2 - Pe*F0*(1+rho/tanh(rho))/2)/(rho*rho*rho*sinh(rho))
 A = F0*(1-Pe-rho*Pe/tanh(rho))/(2*rho*rho*rho*sinh(rho))
 RHS = (Pe*F0*kappa*(1-xi*xi)/(4))*(F0*(1-xi*xi-2/(rho*rho))/(4*rho*rho))# 
 RHS += Pe*F0*kappa*A*(1-xi*xi)*cosh(rho*xi)/4 
@@ -28,23 +26,21 @@ sol +=  Pe*Pe*F0*F0*kappa/(4*rho*rho*rho*rho)*(1/(2*rho*rho*rho*rho) + 36*cosh(r
 sol -= Pe*Pe*F0*F0*kappa/(4*rho*rho*rho*rho)*(1/(2*rho*rho) + cosh(rho*xi)/(rho*sinh(rho)) + xi*sinh(rho*xi)/(2*sinh(rho)) )
 sol += -(F0**2*Pe*kappa*(-3*Pe*rho**2/sinh(rho)**2 + 11*Pe*rho/tanh(rho) + 45*Pe + 3*rho/tanh(rho) + 5)/(4*rho**6))*cosh(sqrt(2)*rho*xi)/(sqrt(2)*rho*sinh(sqrt(2)*rho))
 difference = simplify(expand(diff(diff(sol, xi), xi) - 2*rho*rho*sol - RHS))
-"""
+print("B2", difference)
 
 # SOLVING FOR B0
-
 RHS1 = Pe*kappa*F0*F0*(1-xi*xi)*(1-xi*xi-2/(rho*rho) + 2*cosh(rho*xi)/(rho*sinh(rho)))/(16*rho*rho)
 RHS2 = Pe*Pe*F0*F0*kappa*(1-xi*xi)*(1/(rho*rho)+xi*sinh(rho*xi)/(2*sinh(rho)) - cosh(rho*xi)/(2*rho*sinh(rho))*(1+rho/tanh(rho)))/(4*rho*rho)
-#RHS2 = Pe*Pe*F0*F0*kappa*(1-xi*xi)*(1/(rho*rho)+xi*sinh(rho*xi)/(2*sinh(rho)))/(4*rho*rho)
-
-#rint(simplify(expand(simplify(Pe*kappa*F0*sol*(1-xi*xi)/4)-RHS1-RHS2)))
-#print(simplify(expand(diff(diff(sol, xi),xi)-RHS)))
-
-#sol1 = Pe*kappa*F0*F0*(xi*xi/2 - xi*xi*xi*xi/6 + xi*xi*xi*xi*xi*xi/30 - xi*xi/(rho*rho) + xi*xi*xi*xi/(6*rho*rho) + 2*cosh(rho*xi)/(rho*rho*rho*sinh(rho)) - 2*xi*xi*cosh(rho*xi)/(rho*rho*rho*sinh(rho)) + 8*xi*sinh(rho*xi)/(rho*rho*rho*rho*sinh(rho))-12*cosh(rho*xi)/(rho*rho*rho*rho*rho*sinh(rho)))/(16*rho*rho)
-#sol2 = Pe*Pe*F0*F0*kappa/(4*rho*rho*rho*rho)*(cosh(rho*xi)/(rho*sinh(rho))*(12/(rho*rho)+3*xi*xi-1-9*xi*tanh(rho*xi)/(rho*rho*rho)-rho*xi*xi*xi*tanh(rho*xi)/2+rho*xi*tanh(rho*xi)/2) +xi*xi*(1-xi*xi/6)/2 + (1+rho/tanh(rho))/(2*rho*rho*rho*sinh(rho))*(cosh(rho*xi)*(xi*xi-1+6/(rho*rho))-4*xi*sinh(rho*xi)/rho))
-#sol2 = Pe*Pe*F0*F0*kappa/(4*rho*rho*rho*rho)*(12*cosh(rho*xi)/(rho*rho*rho*sinh(rho)) - 9*xi*sinh(rho*xi)/(rho*rho*sinh(rho))+3*xi*xi*cosh(rho*xi)/(rho*sinh(rho))-cosh(rho*xi)/(rho*sinh(rho))-xi*xi*xi*xi/12-xi*xi*xi*sinh(rho*xi)/(2*sinh(rho))+xi*xi/2+xi*sinh(rho*xi)/(2*sinh(rho)) + (1+rho/tanh(rho))/(2*rho*sinh(rho))*(6*cosh(rho*xi)/(rho*rho) - 4*xi*sinh(rho*xi)/rho + xi*xi*cosh(rho*xi)-cosh(rho*xi)))
 sol1 = Pe*F0*F0*kappa/(16*rho*rho)*((xi*xi/2)*(1-2/(rho*rho))+xi*xi*xi*xi*(1/(rho*rho)-1)/6 + xi*xi*xi*xi*xi*xi/30 + 2*cosh(rho*xi)/(rho*rho*rho*sinh(rho))*(1-xi*xi-6/(rho*rho)) + 8*xi*sinh(rho*xi)/(rho*rho*rho*rho*sinh(rho)))
-print(simplify(expand(simplify(diff(diff(sol1, xi), xi)-(RHS1)))))
+sol2 = Pe*Pe*F0*F0*kappa/(4*rho*rho*rho*rho)*(cosh(rho*xi)/(rho*sinh(rho))*(15/(rho*rho)+7*xi*xi/2-3/2) + xi*sinh(rho*xi)/(2*sinh(rho))*(1-xi*xi-22/(rho*rho)-4/(rho*tanh(rho)))+xi*xi*(1-xi*xi/6)/2  + cosh(rho*xi)/(2*tanh(rho)*sinh(rho))*(xi*xi-1+6/(rho*rho)) )
+
+print("B0 partic: ", simplify(expand(simplify(diff(diff(sol2+sol1, xi), xi)-(RHS2+RHS1)))))
+A = -kappa*(5*F0**2*Pe**2*rho**2/12 - F0**2*Pe**2*rho**2/tanh(rho)**2/4 - 3/4*F0**2*Pe**2*rho/tanh(rho) + F0**2*Pe**2 + F0**2*Pe*rho**4/30 - F0**2*Pe*rho**2/12 + F0**2*Pe*rho/tanh(rho)/4 - F0**2*Pe/4 + rho**6)/rho**6
+sol3 = A*cosh(kappa*xi)/(kappa*sinh(kappa))
+print("B0 homo: ", simplify(expand(simplify(diff(diff(sol3, xi), xi)-kappa*kappa*sol3))))
+
+sol2 += sol3
+print("BOUNDARY CONDITIONS B0 at xi=1:  ", simplify(expand(simplify((diff(sol1+sol2, xi).subs(xi,  1))))))
+print("BOUNDARY CONDITIONS B0 at xi=-1: ", simplify(expand(simplify((diff(sol1+sol2, xi).subs(xi, -1))))))
 
 
-sol2 = Pe*Pe*F0*F0*kappa/(4*rho*rho*rho*rho)*(cosh(rho*xi)/(rho*sinh(rho))*(12/(rho*rho)+3*xi*xi-1) + xi*sinh(rho*xi)/(2*sinh(rho))*(1-xi*xi-18/(rho*rho))+xi*xi*(1-xi*xi/6)/2  - xi*sinh(rho*xi)/sinh(rho)*2*(1+rho/tanh(rho))/(rho*rho) + (1+rho/tanh(rho))*cosh(rho*xi)/(2*rho*sinh(rho))*(xi*xi-1+6/(rho*rho)) )
-print(simplify(expand(simplify(diff(diff(sol2, xi), xi)-(RHS2)))))
