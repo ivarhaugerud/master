0d59ced4000840dcce067707d9c38ae79ff63db9
ivarhaugerud <ivarsh@student.matnat.uio.no>
2021-03-22 11:39:39 +0100

    my approximate solution works I think

--- geometry_calculation/plot_resonance.py
+++ geometry_calculation/plot_resonance.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import scipy.integrate as sci
 
 kappa = np.array([0.25, 0.5, 1.0, 2, 4, 8])
-Lx = 2*np.pi/kappa
-Lx = [25.13, 12.56, 6.283, 3.141, 1.57, 0.785]
 tau = np.array([0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8])
-base = "data_test/find_resonance/"
+Lx = [25.13, 12.56, 6.283, 3.141, 1.57, 0.785]
+
+base = "data_test/find_resonance_try2/"
 D = np.zeros((len(kappa), len(tau)))
 difference = np.zeros(np.shape(D))
-dt = tau/750
+dt = tau/500
 #dt[1:5] = tau[1:5]/500
 Ts = np.ones(len(tau), dtype="int")*int(750)
 #Ts[1:5] = np.ones(len(tau[1:5]), dtype="int")*int(500)
@@ -17,18 +17,19 @@ Ts = np.ones(len(tau), dtype="int")*int(750)
 for i in range(len(kappa)):
 	for j in range(len(tau)):
 		T = Ts[j]
-		data = np.loadtxt(base+"Lx" +  str(Lx[i]) +"_tau"+ str(round(tau[j], 3)) +"_eps0.5_nu5.0_D3.0_fzero0.0_fone100.0_res150_dt" + str(round(dt[j], 6)) + "/tdata.dat")
+		data = np.loadtxt(base+"Lx" +  str(Lx[i]) +"_tau"+ str(round(tau[j], 3)) +"_eps0.5_nu1.2_D1.0_fzero0.0_fone12.0_res150_dt" + str(round(dt[j], 6)) + "/tdata.dat")
 		print(np.shape(data), np.shape(D))
 		D[i, j] = sci.trapz(  data[:, 8][-T:],  data[:, 0][-T:] )/tau[j]
-		#difference[i, j] = abs(D[i, j] - sci.trapz(  np.trim_zeros(data[:, 8])[-2*T:-T],  np.trim_zeros(data[:, 0])[-2*T:-T] )/tau[j])/D[i,j]
+		difference[i, j] = abs(D[i, j] - sci.trapz(  np.trim_zeros(data[:, 8])[-2*T:-T],  np.trim_zeros(data[:, 0])[-2*T:-T] )/tau[j])/D[i,j]
 		#plt.plot(np.trim_zeros(data[:, 0])/tau[j], np.trim_zeros(data[:, 8]))
 		#plt.plot(np.trim_zeros(data[:, 0])[-T:]/tau[j], np.trim_zeros(data[:, 8])[-T:])
-	#plt.show()
+		#plt.title(str(kappa[i]) + ","+ str(tau[j]))
+		#plt.show()
 
 for i in range(len(tau)):
-	plt.plot(kappa, D[:, i], "o", label=str(tau[i]))
+	plt.plot(kappa, D[:, i], label=str(tau[i]))
 
-plt.xscale("log")
+#plt.xscale("log")
 plt.yscale("log")
 plt.legend(loc="best")
 plt.show()
--- geometry_calculation/plot_taylor_approximation.py
+++ geometry_calculation/plot_taylor_approximation.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import scipy.integrate as sci
 
 
-base = "data_test/taylor_approximation/"
-tau  = np.logspace(-2, 2, 10)
+base = "data_test/taylor_approximation_try2/taylor_approximation_try2/"
+tau = np.logspace(-1, 2, 5)
+
 T    = 500
+kappa = 0.2
 dt   = tau/T
 D  = np.zeros((len(tau)))
 U  = np.zeros(np.shape(D))
@@ -13,7 +15,7 @@ difference = np.zeros(np.shape(D))
 D_ana = np.zeros(len(tau))
 
 nu = 8000
-epsilon = 0.4
+epsilon = 0.2
 Dm = 0.1
 F0 = 96000/nu 
 omega = 2*np.pi/tau 
@@ -27,42 +29,68 @@ for i in range(len(tau)):
 	gamma = np.sqrt(1j*omega[i]/nu)
 	D_ana[i] = 0.5*F0**2*Pe**2*(0.583333333333333*rho**8*conjugate(rho)**2 + 0.5*rho**8 + 0.5*rho**8*conjugate(rho)**3/tanh(conjugate(rho)) + 1.75*rho**8*conjugate(rho)/tanh(conjugate(rho)) - 1.75*rho**8*conjugate(rho)**2/tanh(conjugate(rho))**2 - 0.5*rho**8*conjugate(rho)**3/tanh(conjugate(rho))**3 - 0.5*rho**7*conjugate(rho)**4/tanh(rho) + 0.5*rho**7*conjugate(rho)**4/tanh(rho)**3 - 2.75*rho**6*conjugate(rho)**4 - 1.0*rho**6*conjugate(rho)**2 - 1.0*rho**6*conjugate(rho)**5/tanh(conjugate(rho)) - 4.0*rho**6*conjugate(rho)**3/tanh(conjugate(rho)) + 4.0*rho**6*conjugate(rho)**4/tanh(conjugate(rho))**2 + 1.0*rho**6*conjugate(rho)**5/tanh(conjugate(rho))**3 + 2.25*rho**6*conjugate(rho)**4/tanh(rho)**2 + 1.0*rho**5*conjugate(rho)**6/tanh(rho) - 0.25*rho**5*conjugate(rho)**4/tanh(rho) - 1.0*rho**5*conjugate(rho)**6/tanh(rho)**3 + 2.75*rho**4*conjugate(rho)**6 + 0.5*rho**4*conjugate(rho)**7/tanh(conjugate(rho)) + 0.25*rho**4*conjugate(rho)**5/tanh(conjugate(rho)) - 2.25*rho**4*conjugate(rho)**6/tanh(conjugate(rho))**2 - 0.5*rho**4*conjugate(rho)**7/tanh(conjugate(rho))**3 - 4.0*rho**4*conjugate(rho)**6/tanh(rho)**2 - 0.5*rho**3*conjugate(rho)**8/tanh(rho) + 4.0*rho**3*conjugate(rho)**6/tanh(rho) + 0.5*rho**3*conjugate(rho)**8/tanh(rho)**3 - 0.583333333333333*rho**2*conjugate(rho)**8 + 1.0*rho**2*conjugate(rho)**6 + 1.75*rho**2*conjugate(rho)**8/tanh(rho)**2 - 1.75*rho*conjugate(rho)**8/tanh(rho) - 0.5*conjugate(rho)**8)/(rho**4*(1.0*rho**6 - 3.0*rho**4*conjugate(rho)**2 + 3.0*rho**2*conjugate(rho)**4 - 1.0*conjugate(rho)**6)*conjugate(rho)**4)
 	D0[i]    = 1 + Pe*Pe*F0*F0*tanh(gamma)*tanh(conjugate(gamma))/(4*gamma*conjugate(gamma)*(gamma**4 - rho**4))*(1/(gamma*gamma)*(gamma/tanh(gamma) - conjugate(gamma/tanh(gamma))) - 1/(rho*rho)*(rho/tanh(rho) - conjugate(rho/tanh(rho))))
+	#D_ana[i] -= -1/(kappa*kappa) + 1/(kappa*tanh(kappa)) + 1
 	try:
-		data = np.loadtxt(base+"Lx31.41_tau" + str(tau[i])[:4] + "_eps0.5_nu8000.0_D0.1_fzero0.0_fone96000.0_res150_dt"+str(dt[i])[:6] +"/tdata.dat")
-		print(np.shape(data), np.shape(D))
+		data = np.loadtxt(base+"Lx31.41_tau" + str(tau[i])[:4] + "_eps0.2_nu8000.0_D0.1_fzero0.0_fone96000.0_res150_dt"+str(dt[i])[:6] +"/tdata.dat")
 		D[i] = sci.trapz(  data[-T:, 8],  data[-T:, 0] )/tau[i]
 		U[i] = sci.trapz(  data[-T:, 4],  data[-T:, 0] )/tau[i]
-	#plt.scatter(nu[i], data[-1, 0]/tau)
 		difference[i] = abs(D[i] - sci.trapz(  data[-2*T:-T, 8],  data[-2*T:-T, 0] )/tau[i])/D[i]
-		plt.plot(np.trim_zeros(data[:, 0])/tau[i], np.trim_zeros(data[:, 8]))
-		plt.plot(np.trim_zeros(data[:, 0])[-T:]/tau[i], np.trim_zeros(data[:, 8])[-T:])
-		plt.show()
-	except:
-		print(tau[i])
+		#plt.plot(np.trim_zeros(data[:, 0])/tau[i], np.trim_zeros(data[:, 8]))
+		#plt.plot(np.trim_zeros(data[:, 0])[-T:]/tau[i], np.trim_zeros(data[:, 8])[-T:])
+		#plt.show()
 	
+	except:
+		data = np.loadtxt(base+"Lx31.41_tau" + str(tau[i])[:5] + "_eps0.2_nu8000.0_D0.1_fzero0.0_fone96000.0_res150_dt"+str(dt[i])[:6] +"/tdata.dat")
+		D[i] = sci.trapz(  data[-T:, 8],  data[-T:, 0] )/tau[i]
+		U[i] = sci.trapz(  data[-T:, 4],  data[-T:, 0] )/tau[i]
+		difference[i] = abs(D[i] - sci.trapz(  data[-2*T:-T, 8],  data[-2*T:-T, 0] )/tau[i])/D[i]
+		#plt.plot(np.trim_zeros(data[:, 0])/tau[i], np.trim_zeros(data[:, 8]))
+		#plt.plot(np.trim_zeros(data[:, 0])[-T:]/tau[i], np.trim_zeros(data[:, 8])[-T:])
+		#plt.show()	
 
 
+omega_contin = np.logspace(np.log10(min(2*np.pi/tau)), np.log10(max(2*np.pi/tau)))
+D0_contin    = np.zeros(len(omega_contin))
+D_ana_contin = np.zeros(len(omega_contin))
 
-plt.plot(omega, D_ana)
-plt.xscale("log")
-plt.show()
+for i in range(len(omega_contin)):
+	rho = np.sqrt(1j*omega_contin[i]/Dm)
+	gamma = np.sqrt(1j*omega_contin[i]/nu)
+	D_ana_contin[i] = 0.5*F0**2*Pe**2*(0.583333333333333*rho**8*conjugate(rho)**2 + 0.5*rho**8 + 0.5*rho**8*conjugate(rho)**3/tanh(conjugate(rho)) + 1.75*rho**8*conjugate(rho)/tanh(conjugate(rho)) - 1.75*rho**8*conjugate(rho)**2/tanh(conjugate(rho))**2 - 0.5*rho**8*conjugate(rho)**3/tanh(conjugate(rho))**3 - 0.5*rho**7*conjugate(rho)**4/tanh(rho) + 0.5*rho**7*conjugate(rho)**4/tanh(rho)**3 - 2.75*rho**6*conjugate(rho)**4 - 1.0*rho**6*conjugate(rho)**2 - 1.0*rho**6*conjugate(rho)**5/tanh(conjugate(rho)) - 4.0*rho**6*conjugate(rho)**3/tanh(conjugate(rho)) + 4.0*rho**6*conjugate(rho)**4/tanh(conjugate(rho))**2 + 1.0*rho**6*conjugate(rho)**5/tanh(conjugate(rho))**3 + 2.25*rho**6*conjugate(rho)**4/tanh(rho)**2 + 1.0*rho**5*conjugate(rho)**6/tanh(rho) - 0.25*rho**5*conjugate(rho)**4/tanh(rho) - 1.0*rho**5*conjugate(rho)**6/tanh(rho)**3 + 2.75*rho**4*conjugate(rho)**6 + 0.5*rho**4*conjugate(rho)**7/tanh(conjugate(rho)) + 0.25*rho**4*conjugate(rho)**5/tanh(conjugate(rho)) - 2.25*rho**4*conjugate(rho)**6/tanh(conjugate(rho))**2 - 0.5*rho**4*conjugate(rho)**7/tanh(conjugate(rho))**3 - 4.0*rho**4*conjugate(rho)**6/tanh(rho)**2 - 0.5*rho**3*conjugate(rho)**8/tanh(rho) + 4.0*rho**3*conjugate(rho)**6/tanh(rho) + 0.5*rho**3*conjugate(rho)**8/tanh(rho)**3 - 0.583333333333333*rho**2*conjugate(rho)**8 + 1.0*rho**2*conjugate(rho)**6 + 1.75*rho**2*conjugate(rho)**8/tanh(rho)**2 - 1.75*rho*conjugate(rho)**8/tanh(rho) - 0.5*conjugate(rho)**8)/(rho**4*(1.0*rho**6 - 3.0*rho**4*conjugate(rho)**2 + 3.0*rho**2*conjugate(rho)**4 - 1.0*conjugate(rho)**6)*conjugate(rho)**4)
+	D0_contin[i]    = 1 + Pe*Pe*F0*F0*tanh(gamma)*tanh(conjugate(gamma))/(4*gamma*conjugate(gamma)*(gamma**4 - rho**4))*(1/(gamma*gamma)*(gamma/tanh(gamma) - conjugate(gamma/tanh(gamma))) - 1/(rho*rho)*(rho/tanh(rho) - conjugate(rho/tanh(rho))))
+
+D_tot_ana = D0 + epsilon*epsilon*D_ana
+D_tot_ana_contin = D0_contin + epsilon*epsilon*D_ana_contin
+
+
 #plt.xscale("log")
 #plt.show()
 plt.figure(1)
 plt.plot(tau, difference, "o")
 plt.ylabel(r"check for convergence")
 plt.xlabel(r"viscosity $\nu$")
+plt.yscale("log")
 plt.xscale("log")
 plt.legend(loc="best")
 
 plt.figure(2)
 plt.plot(tau, D, "o")
-#plt.plot(tau, D0)
-#plt.plot(tau, epsilon*epsilon*D_ana)
-plt.plot(tau, D0 + epsilon*epsilon*D_ana)
+#plt.plot(tau, D0 + epsilon*epsilon*D_ana)
+plt.plot(2*np.pi/omega_contin, D_tot_ana_contin)
 plt.ylabel(r"$D_{eff}$")
 plt.xlabel(r"viscosity $\nu$")
 
+plt.xscale("log")
+plt.legend(loc="best")
+plt.show()
+
+
+
+plt.figure(2)
+plt.plot(tau, abs(D_tot_ana-D)/D)
+plt.ylabel(r"$D_{eff}$")
+plt.xlabel(r"viscosity $\nu$")
+plt.plot(tau, np.ones(len(tau))*kappa**2)
 plt.xscale("log")
 plt.legend(loc="best")
 plt.show()
