be76ab5c2465b9b8aeb46fdc009ad9940f571147
ivarhaugerud <ivarsh@student.matnat.uio.no>
2021-04-06 10:35:14 +0200

    plotted more

--- geometry_calculation/finished_plots/plot_smallgrad.py
+++ geometry_calculation/finished_plots/plot_smallgrad.py
@@ -16,7 +16,7 @@ root = "../../../master_latex/results/"
 Lx = np.array([41.89])#, 62.83])#, 41.89])
 kappa = 2*np.pi/Lx
 epsilon = np.array([0.0, 0.0178, 0.0316, 0.056, 0.1, 0.177, 0.245, 0.31])
-base = "data_test/benchmark_3/"
+base = "../data_test/benchmark_3/"
 
 D          = np.zeros((len(epsilon), len(Lx)))
 difference = np.zeros(np.shape(D))
@@ -54,7 +54,7 @@ from numpy import *
 D2  = 1/2*F0**2*Pe**2*(7/12*rho**8*conjugate(rho)**2 + rho**8/2 + rho**8*conjugate(rho)**3/(2*tanh(conjugate(rho))) + 7/4*rho**8*conjugate(rho)/tanh(conjugate(rho)) - 7/4*rho**8*conjugate(rho)**2/tanh(conjugate(rho))**2 - 1/2*rho**8*conjugate(rho)**3/tanh(conjugate(rho))**3 - 1/2*rho**7*conjugate(rho)**4/tanh(rho) + 1/2*rho**7*conjugate(rho)**4/tanh(rho)**3 - 11/4*rho**6*conjugate(rho)**4 - rho**6*conjugate(rho)**2 - rho**6*conjugate(rho)**5/tanh(conjugate(rho)) - 4*rho**6*conjugate(rho)**3/tanh(conjugate(rho)) + 4*rho**6*conjugate(rho)**4/tanh(conjugate(rho))**2 + rho**6*conjugate(rho)**5/tanh(conjugate(rho))**3 + 9/4*rho**6*conjugate(rho)**4/tanh(rho)**2 + rho**5*conjugate(rho)**6/tanh(rho) - 1/4*rho**5*conjugate(rho)**4/tanh(rho) - rho**5*conjugate(rho)**6/tanh(rho)**3 + 11/4*rho**4*conjugate(rho)**6 + 1/2*rho**4*conjugate(rho)**7/tanh(conjugate(rho)) + 1/4*rho**4*conjugate(rho)**5/tanh(conjugate(rho)) - 9/4*rho**4*conjugate(rho)**6/tanh(conjugate(rho))**2 - 1/2*rho**4*conjugate(rho)**7/tanh(conjugate(rho))**3 - 4*rho**4*conjugate(rho)**6/tanh(rho)**2 - 1/2*rho**3*conjugate(rho)**8/tanh(rho) + 4*rho**3*conjugate(rho)**6/tanh(rho) + 1/2*rho**3*conjugate(rho)**8/tanh(rho)**3 - 7/12*rho**2*conjugate(rho)**8 + rho**2*conjugate(rho)**6 + 7/4*rho**2*conjugate(rho)**8/tanh(rho)**2 - 7/4*rho*conjugate(rho)**8/tanh(rho) - 1/2*conjugate(rho)**8)/(rho**4*(rho**6 - 3*rho**4*conjugate(rho)**2 + 3*rho**2*conjugate(rho)**4 - conjugate(rho)**6)*conjugate(rho)**4)
 D2 += -1/(kappa*kappa) + 1/(kappa*tanh(kappa)) + 1 #approx 8/3 + O(kappa^2)
 DA = D[0,0] + epsilon*epsilon*D2
-D_ana = np.load("finite_element/data/vary_kappa_small.npy")
+D_ana = np.load("../finite_element/data/vary_kappa_small.npy")
 """
 for i in range(len(kappa)):
 	plt.plot(epsilon, difference[:,i])
@@ -64,32 +64,33 @@ plt.show()
 """
 
 for j in range(len(kappa)):
-	plt.plot(epsilon, D0 + epsilon*epsilon*D_ana[j])
-	plt.plot(epsilon, D[:,j], "o", markersize=3)
-	plt.plot(epsilon, DA, "ro", markersize=3)
-#plt.xscale("log")
-plt.xlabel(r" Womersley number $\frac{\omega a^2}{\nu}$", fontsize=8)
-plt.ylabel(r" Effective Diffusion Coefficient $ D_\parallel $",  fontsize=8)
+	plt.plot(epsilon, D[:,j]-1, "-", label="Numeric")
+	plt.plot(epsilon, D[0,0]-1 + epsilon*epsilon*D_ana[j], "o", markersize=3, label="Semi-analytic")
+	plt.plot(epsilon, DA-1, "o", markersize=3, label="Analytic")
+plt.xscale("log")
+plt.yscale("log")
+
+plt.xlabel(r" Boundary amplitude $\epsilon$", fontsize=8)
+plt.ylabel(r" Effective Diffusion Coefficient $ D_\parallel-1$",  fontsize=8)
 plt.legend(loc="best", fontsize=8)
 plt.tick_params(axis='both', which='major', labelsize=8)
 plt.tick_params(axis='both', which='minor', labelsize=8)
-#filename = root + "figures/D_eff_vs_nu.pdf"
-#plt.savefig(filename, bbox_inches="tight")
-#os.system('pdfcrop %s %s &> /dev/null &'%(filename, filename))
+filename = root + "figures/D_eff_ana_and_num.pdf"
+plt.savefig(filename, bbox_inches="tight")
+os.system('pdfcrop %s %s &> /dev/null &'%(filename, filename))
 plt.show()
 
 
 
 for j in range(len(kappa)):
 	plt.plot(epsilon, abs(D[:,j]-(D[0,0] + epsilon*epsilon*D_ana[j]))/D[:,j], "o", markersize=3, label="Semi-analytic")
-	plt.plot(epsilon, abs(D[:,j]-DA), "o", markersize=3, label="Analytic")
+	#plt.plot(epsilon, abs(D[:,j]-DA), "o", markersize=3, label="Analytic")
 
 epsilon = np.linspace(0.01, 0.35, 1000)
 gamma = np.real(gamma)
-plt.plot(epsilon, epsilon**4, label=r"$\epsilon^4$")
-plt.plot(epsilon, epsilon**2, label=r"$\epsilon^2$")
-plt.plot(epsilon, epsilon*epsilon*kappa*kappa, label=r"$\epsilon^2\kappa^2$")
-plt.plot(epsilon, epsilon*epsilon*gamma*gamma, label=r"$\epsilon^2\gamma^2$")
+plt.plot(epsilon, epsilon**4/(1-epsilon), label=r"$\epsilon^4$")
+plt.plot(epsilon, epsilon**2/(1-epsilon), label=r"$\epsilon^2$")
+#plt.plot(epsilon, epsilon*epsilon*(gamma*gamma+kappa*kappa), label=r"$\epsilon^2(\gamma^2+\kappa^2)$")
 plt.xlabel(r" Boundary amplitude $\epsilon$", fontsize=8)
 plt.ylabel(r" Relative difference $ D_\parallel $",  fontsize=8)
 plt.legend(loc="best", fontsize=8)
@@ -97,7 +98,32 @@ plt.yscale("log")
 plt.xscale("log")
 plt.tick_params(axis='both', which='major', labelsize=8)
 plt.tick_params(axis='both', which='minor', labelsize=8)
-#filename = root + "figures/D_eff_vs_nu.pdf"
-#plt.savefig(filename, bbox_inches="tight")
-#os.system('pdfcrop %s %s &> /dev/null &'%(filename, filename))
+filename = root + "figures/rel_diff_semi_analytic.pdf"
+plt.savefig(filename, bbox_inches="tight")
+os.system('pdfcrop %s %s &> /dev/null &'%(filename, filename))
+plt.show()
+
+
+
+
+epsilon = np.array([0.0, 0.0178, 0.0316, 0.056, 0.1, 0.177, 0.245, 0.31])
+for j in range(len(kappa)):
+	#plt.plot(epsilon, abs(D[:,j]-(D[0,0] + epsilon*epsilon*D_ana[j]))/D[:,j], "o", markersize=3, label="Semi-analytic")
+	plt.plot(epsilon, abs(D[:,j]-DA), "o", markersize=3, label="Analytic")
+epsilon = np.linspace(0.01, 0.35, 1000)
+gamma = np.real(gamma)
+#plt.plot(epsilon, epsilon**4/(1-epsilon), label=r"$\epsilon^4$")
+plt.plot(epsilon, epsilon**2/(1-epsilon), label=r"$\epsilon^2$")
+plt.plot(epsilon, epsilon**4/(1-epsilon)+epsilon*epsilon*(gamma*gamma+kappa*kappa), label=r"$\epsilon^4+\epsilon^2(\gamma^2+\kappa^2)$")
+plt.xlabel(r" Boundary amplitude $\epsilon$", fontsize=8)
+plt.ylabel(r" Relative difference $ D_\parallel $",  fontsize=8)
+plt.legend(loc="best", fontsize=8)
+plt.yscale("log")
+plt.xscale("log")
+plt.tick_params(axis='both', which='major', labelsize=8)
+plt.tick_params(axis='both', which='minor', labelsize=8)
+plt.axis([0.01, 0.40, 1.8*1e-6, 0.38])
+filename = root + "figures/rel_diff_analytic.pdf"
+plt.savefig(filename, bbox_inches="tight")
+os.system('pdfcrop %s %s &> /dev/null &'%(filename, filename))
 plt.show()
